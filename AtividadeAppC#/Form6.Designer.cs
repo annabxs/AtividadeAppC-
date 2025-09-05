3428eeb54ad99b359c2562f691654ce130d30b76
Caio <caio.balassoni1510@gmail.com>
2025-09-05 17:13:13 -0300

    Exercício 5

--- AtividadeAppC#/Form6.Designer.cs
+++ AtividadeAppC#/Form6.Designer.cs
@@ -28,21 +28,128 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnvoltar = new System.Windows.Forms.Button();
+            this.btnlimpar = new System.Windows.Forms.Button();
+            this.btncalcular = new System.Windows.Forms.Button();
+            this.txtresultado = new System.Windows.Forms.TextBox();
+            this.txtaltura = new System.Windows.Forms.TextBox();
+            this.lblaltura = new System.Windows.Forms.Label();
+            this.txtpeso = new System.Windows.Forms.TextBox();
+            this.lblpeso = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // btnvoltar
+            // 
+            this.btnvoltar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.btnvoltar.Location = new System.Drawing.Point(390, 213);
+            this.btnvoltar.Name = "btnvoltar";
+            this.btnvoltar.Size = new System.Drawing.Size(125, 41);
+            this.btnvoltar.TabIndex = 23;
+            this.btnvoltar.Text = "Voltar";
+            this.btnvoltar.UseVisualStyleBackColor = true;
+            this.btnvoltar.Click += new System.EventHandler(this.btnvoltar_Click);
+            // 
+            // btnlimpar
+            // 
+            this.btnlimpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.btnlimpar.Location = new System.Drawing.Point(233, 213);
+            this.btnlimpar.Name = "btnlimpar";
+            this.btnlimpar.Size = new System.Drawing.Size(125, 41);
+            this.btnlimpar.TabIndex = 22;
+            this.btnlimpar.Text = "Limpar";
+            this.btnlimpar.UseVisualStyleBackColor = true;
+            this.btnlimpar.Click += new System.EventHandler(this.btnlimpar_Click);
+            // 
+            // btncalcular
+            // 
+            this.btncalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.btncalcular.Location = new System.Drawing.Point(74, 213);
+            this.btncalcular.Name = "btncalcular";
+            this.btncalcular.Size = new System.Drawing.Size(125, 41);
+            this.btncalcular.TabIndex = 21;
+            this.btncalcular.Text = "Calcular";
+            this.btncalcular.UseVisualStyleBackColor = true;
+            this.btncalcular.Click += new System.EventHandler(this.btncalcular_Click);
+            // 
+            // txtresultado
+            // 
+            this.txtresultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.txtresultado.Location = new System.Drawing.Point(74, 159);
+            this.txtresultado.Name = "txtresultado";
+            this.txtresultado.ReadOnly = true;
+            this.txtresultado.Size = new System.Drawing.Size(680, 30);
+            this.txtresultado.TabIndex = 20;
+            // 
+            // txtaltura
+            // 
+            this.txtaltura.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.txtaltura.Location = new System.Drawing.Point(329, 38);
+            this.txtaltura.Name = "txtaltura";
+            this.txtaltura.Size = new System.Drawing.Size(100, 30);
+            this.txtaltura.TabIndex = 18;
+            this.txtaltura.TextChanged += new System.EventHandler(this.txtaltura_TextChanged);
+            // 
+            // lblaltura
+            // 
+            this.lblaltura.AutoSize = true;
+            this.lblaltura.Font = new System.Drawing.Font("Microsoft JhengHei UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblaltura.Location = new System.Drawing.Point(69, 41);
+            this.lblaltura.Name = "lblaltura";
+            this.lblaltura.Size = new System.Drawing.Size(254, 25);
+            this.lblaltura.TabIndex = 17;
+            this.lblaltura.Text = "Insira a altura(em metros):";
+            this.lblaltura.Click += new System.EventHandler(this.lblaltura_Click);
+            // 
+            // txtpeso
+            // 
+            this.txtpeso.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.txtpeso.Location = new System.Drawing.Point(329, 74);
+            this.txtpeso.Name = "txtpeso";
+            this.txtpeso.Size = new System.Drawing.Size(100, 30);
+            this.txtpeso.TabIndex = 25;
+            this.txtpeso.TextChanged += new System.EventHandler(this.txtpeso_TextChanged);
+            // 
+            // lblpeso
+            // 
+            this.lblpeso.AutoSize = true;
+            this.lblpeso.Font = new System.Drawing.Font("Microsoft JhengHei UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblpeso.Location = new System.Drawing.Point(181, 74);
+            this.lblpeso.Name = "lblpeso";
+            this.lblpeso.Size = new System.Drawing.Size(136, 25);
+            this.lblpeso.TabIndex = 24;
+            this.lblpeso.Text = "Insira o peso:";
+            this.lblpeso.Click += new System.EventHandler(this.lblpeso_Click);
             // 
             // frmexercicio5
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1067, 554);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtpeso);
+            this.Controls.Add(this.lblpeso);
+            this.Controls.Add(this.btnvoltar);
+            this.Controls.Add(this.btnlimpar);
+            this.Controls.Add(this.btncalcular);
+            this.Controls.Add(this.txtresultado);
+            this.Controls.Add(this.txtaltura);
+            this.Controls.Add(this.lblaltura);
             this.Name = "frmexercicio5";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form6";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnvoltar;
+        private System.Windows.Forms.Button btnlimpar;
+        private System.Windows.Forms.Button btncalcular;
+        private System.Windows.Forms.TextBox txtresultado;
+        private System.Windows.Forms.TextBox txtaltura;
+        private System.Windows.Forms.Label lblaltura;
+        private System.Windows.Forms.TextBox txtpeso;
+        private System.Windows.Forms.Label lblpeso;
     }
 }
--- AtividadeAppC#/Form6.cs
+++ AtividadeAppC#/Form6.cs
@@ -16,5 +16,64 @@ namespace AtividadeAppC_
         {
             InitializeComponent();
         }
+
+        private void lblpeso_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtpeso_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtaltura_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblaltura_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btncalcular_Click(object sender, EventArgs e)
+        {
+            double peso, altura, imc;
+
+            peso = Convert.ToDouble(txtpeso.Text);
+            altura = Convert.ToDouble(txtaltura.Text);
+
+            imc = Math.Round(peso / Math.Pow(altura, 2), 2);
+
+            if (imc < 17)
+                txtresultado.Text = "Seu IMC é de " + imc + " e se enquadra em magreza severa.";
+            else if (imc >= 17 && imc < 18.5)
+                txtresultado.Text = "Seu IMC é de " + imc + " e se enquadra em magreza leve.";
+            else if (imc >= 18.5 && imc < 25)
+                txtresultado.Text = "Seu IMC é de " + imc + " e se enquadra em peso normal.";
+            else if (imc >= 25 && imc < 30)
+                txtresultado.Text = "Seu IMC é de " + imc + " e se enquadra em sobrepeso.";
+            else if (imc >= 30 && imc < 35)
+                txtresultado.Text = "Seu IMC é de " + imc + " e se enquadra em obesidade classe I.";
+            else if (imc >= 35 && imc < 40)
+                txtresultado.Text = "Seu IMC é de " + imc + " e se enquadra em obesidade classe II.";
+            else
+                txtresultado.Text = "Seu IMC é de " + imc + " e se enquadra em obesidade classe III.";
+        }
+
+        private void btnlimpar_Click(object sender, EventArgs e)
+        {
+            txtpeso.Clear();
+            txtaltura.Clear();
+            txtresultado.Clear();
+        }
+
+        private void btnvoltar_Click(object sender, EventArgs e)
+        {
+            frmmenu menu = new frmmenu();
+            menu.Show();
+            Hide();
+        }
     }
 }
